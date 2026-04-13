2cab9fac9b158bd57719e1e39a82644b0567b1a7
Krishna Chaitanya <chaitanyabezawada9@gmail.com>
2026-04-13 16:13:47 +0530

    Add files via upload

---
+++ pra.py
@@ -0,0 +1,11 @@
+def is_armstrong(num):
+    n=len(str(num))
+    sum=0
+    for digit in str(num):
+        sum +=int(digit)**n
+    return num==sum
+num =int(input(" "))
+if is_armstrong(num):
+    print(f"{num} is armstrong number.")
+else:
+    print(f"{num} is not armstrong number.")
